56929236f8448f19ead0a16ba0211bab5f47869a
aterrero <51141052+aterrero@users.noreply.github.com>
2019-06-18 18:38:30 -0400

    Update masternfsconfigurator.py

--- masternfsconfigurator.py
+++ masternfsconfigurator.py
@@ -12,9 +12,13 @@ sip2.remove('')
 os.system(ufw enable)
 os.system(ufw allow ssh)
 
+os.system("mkdir /var/nfs/general -p")
+os.system("chown nobody:nogroup /var/nfs/general")
+
 newconfig = "/var/nfs/general" + " " + " " + " "
 for i in range(len(sip2)):
     newconfig = newconfig + "  {}\(rw,sync,no_subtre_check\)".format(sip2[i])
     os.system("ufw allow from {} to any port nfs".format(sip2[i]))
 
 os.system("echo {} >> /etc/exports".format(newconfig))
+os.system("systemctl restart nfs-kernel-server")
