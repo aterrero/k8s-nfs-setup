24f2c9fecd3a63e332ef43ca1224c36aa3c0c18e
aterrero <51141052+aterrero@users.noreply.github.com>
2019-06-18 18:40:31 -0400

    Update workernfsconfigurator.py

--- workernfsconfigurator.py
+++ workernfsconfigurator.py
@@ -4,5 +4,7 @@ mf = open("manager","r")
 mip = mf.read().strip()
 mf.close()
 
+os.system("apt install nfs-common")
+
 os.system("mkdir -p /nfs/general")
 os.system("mount {}:/var/nfs/general /nfs/general".format(mip))
