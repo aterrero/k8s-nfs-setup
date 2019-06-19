37246bbc5d4738f2c4a783fdb1ca36f37935893a
aterrero <51141052+aterrero@users.noreply.github.com>
2019-06-19 02:48:00 -0400

    Create masternfsconfigurator.py

---
+++ masternfsconfigurator.py
@@ -0,0 +1,27 @@
+import os
+
+sf = open("workers","r")
+sip = sf.read().replace("-","")
+sf.close()
+
+sip2 = sip.split('\n')
+sip2.remove('')
+sip2.remove('')
+sip2.remove('')
+
+os.system("apt update")
+os.system("apt -y install nfs-kernel-server")
+
+os.system("ufw allow ssh")
+os.system("ufw enable")
+
+os.system("mkdir /var/nfs/general -p")
+os.system("chown nobody:nogroup /var/nfs/general")
+
+newconfig = "/var/nfs/general" + " " + " " + " "
+for i in range(len(sip2)):
+    newconfig = newconfig + "  {}\(rw,sync,no_subtree_check,no_root_squash\)".format(sip2[i])
+    os.system("ufw allow from {} to any port nfs".format(sip2[i]))
+
+os.system("echo {} >> /etc/exports".format(newconfig))
+os.system("systemctl restart nfs-kernel-server")
