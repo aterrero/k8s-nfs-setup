8d0c048119ad4a82f723795b4104502a63c62260
aterrero <51141052+aterrero@users.noreply.github.com>
2019-06-19 04:24:12 -0400

    Update workernfsconfigurator.py

--- workernfsconfigurator.py
+++ workernfsconfigurator.py
@@ -8,5 +8,6 @@ os.system("apt update")
 os.system("apt -y install nfs-common")
 
 os.system("mkdir -p /root/shared")
+os.system("chmod 777 /root/shared")
 print("directory created")
 os.system("mount {}:/root/shared /root/shared".format(mip))
