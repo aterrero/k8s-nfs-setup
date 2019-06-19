d916dcf363dc6f95178c38d138c704bd99dd79c9
aterrero <51141052+aterrero@users.noreply.github.com>
2019-06-19 04:22:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#Docker and Kubernetes
 python3 setup.py
 kubeadm init --ignore-preflight-errors='all' --pod-network-cidr=10.244.0.0/16
 kubeadm token create --print-join-command > joincommand.sh
@@ -8,7 +9,6 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-python3 masternfsconfigurator.py
 
 cat workers | while read line
 do
@@ -19,9 +19,20 @@ do
         scp setup.py root@$line:/root
         scp master root@$line:/root
         scp workernfsconfigurator.py root@$line:/root
-        ssh root@$line -n "cd /root && python3 setup.py && ./joincommand.sh && python3 workernfsconfigurator.py"
-        echo "Finished config node $line"
+        ssh root@$line -n "cd /root && python3 setup.py && ./joincommand.sh"
     fi
 done
+
+#NFS
+python3 masternfsconfigurator.py
+cat workers | while read line
+do
+    if [ "$line" = "-" ]; then
+        echo "Skip $line"
+    else
+        ssh root@$line -n "cd /root && python3 workernfsconfigurator.py"
+    fi
+done
+
 
 echo "Finished Installation"
