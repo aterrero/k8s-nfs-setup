e27468aee03f4879e2c29d028834e2697faafd6c
aterrero <51141052+aterrero@users.noreply.github.com>
2019-07-08 21:11:04 -0400

    Update masternfsconfigurator.py

--- masternfsconfigurator.py
+++ masternfsconfigurator.py
@@ -12,12 +12,9 @@ sip2.remove('')
 os.system("apt update")
 os.system("apt -y install nfs-kernel-server")
 
-os.system("ufw allow ssh")
-os.system("ufw --force enable")
-
 os.system("mkdir /root/shared -p")
-os.system("chmod 777 /root/shared")
 os.system("chown nobody:nogroup /root/shared")
+os.system("chmod 777 /root/shared")
 
 newconfig = "/root/shared" + " " + " " + " "
 for i in range(len(sip2)):
@@ -25,4 +22,8 @@ for i in range(len(sip2)):
     os.system("ufw allow from {} to any port nfs".format(sip2[i]))
 
 os.system("echo {} >> /etc/exports".format(newconfig))
+os.system("exportfs -a")
 os.system("systemctl restart nfs-kernel-server")
+
+#os.system("ufw allow ssh")
+#os.system("ufw --force enable")
